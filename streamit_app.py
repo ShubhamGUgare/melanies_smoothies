6f713df40a9763cf13e7ff35809ddd547a36197f
ShubhamGUgare <134708571+ShubhamGUgare@users.noreply.github.com>
2024-08-06 15:31:09 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -70,11 +70,12 @@ if ingredients_list:
 
 import requests
 if ingredients_list:
- 
  ingredients_string=''
 
-for fruit_chosen in ingredents_list:
+ for fruit_chosen in ingredents_list:
+   ingredients_string+=fruit_chosen +''
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+   fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+  
  
- ingredients_string+=fruit_chosen +''
- fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
- fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+ 
